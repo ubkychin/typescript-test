f09208ada53e2a6c8d9781276f9db5402263c97f
ubkychin <anguyen@swin.edu.au>
2021-07-19 01:52:31 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -1,8 +1,2 @@
 // Import stylesheets
 import './style.css';
-
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
-console.log("hello");
